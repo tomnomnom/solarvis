dcbefd6c4fdde4f9bad64928ead9ae097d59700b
Tom Hudson <mail@tomnomnom.com>
2025-06-11 22:50:39 +0100

    Tweaked

--- index.php
+++ index.php
@@ -10,7 +10,38 @@
         </style>
     </head>
     <body>
+        <!--<audio src=drone.mp3 id=sunsound></audio>-->
         <canvas width=1200 height=600 id=stage></canvas>
         <script src=main.js></script>
+        <script>
+            const stage = document.getElementById("stage");
+            //const sunSound = document.getElementById("sunsound");
+
+            stage.addEventListener("click", e => {
+                //sunSound.paused ? sunSound.play() : sunSound.pause();
+            });
+
+            const state = {
+                fullScreen: false
+            };
+
+            window.addEventListener('keydown', e => {
+                switch (e.key) {
+
+                case 'f':
+                    if (!state.fullScreen) {
+                        document.documentElement.requestFullscreen()
+                    } else {
+                        document.exitFullscreen()
+                    }
+                    state.fullScreen = !state.fullScreen
+                    break
+
+                case 'p':
+                    //sunSound.paused ? sunSound.play() : sunSound.pause();
+                    break
+                }
+            });
+        </script>
     </body>
 </html>
--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 navigator.mediaDevices.getUserMedia({audio: true}).then(function(stream){
-	const audioCtx = new AudioContext();
+    const audioCtx = new AudioContext();
 
-	const analyser = audioCtx.createAnalyser();
-	analyser.fftSize = 4096;
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 4096;
+    //analyser.connect(audioCtx.destination);
 
-	const microphone = audioCtx.createMediaStreamSource(stream);
-	microphone.connect(analyser);
+    //const sunSound = document.getElementById("sunsound");
+    //const sun = audioCtx.createMediaElementSource(sunSound);
+    //sun.connect(analyser);
 
-	const bufferLength = analyser.frequencyBinCount;
-	const freqData = new Uint8Array(bufferLength);
+    const microphone = audioCtx.createMediaStreamSource(stream);
+    microphone.connect(analyser);
 
-	const canvas = document.getElementById("stage");
-	const ctx = canvas.getContext("2d");
+    const bufferLength = analyser.frequencyBinCount;
+    const freqData = new Uint8Array(bufferLength);
+
+    const canvas = document.getElementById("stage");
+    const ctx = canvas.getContext("2d");
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -23,48 +28,47 @@ navigator.mediaDevices.getUserMedia({audio: true}).then(function(stream){
 
     let nudgeFactor = 1000;
 
-	function drawFrame() {
-		requestAnimationFrame(drawFrame);
+    function drawFrame() {
+        requestAnimationFrame(drawFrame);
 
-		analyser.getByteFrequencyData(freqData);
+        analyser.getByteFrequencyData(freqData);
 
-		ctx.fillStyle = "hsl(0 0 10)";
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "hsl(0 0 10)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         const maxSize = Math.min(canvas.width, canvas.height);
         const sunRadius = maxSize / 3;
 
         ctx.save()
         ctx.translate(canvas.width/2, canvas.height/2);
-        ctx.shadowBlur = 10;
+        ctx.shadowBlur = 15;
 
         ctx.beginPath();
 
         let totalHeight = 0;
 
         for (let i = 0; i < bufferLength; i++) {
-            let barHeight = (freqData[i] / 255) * sunRadius / 32;
-            barHeight = barHeight**2.5;
-            barHeight += Math.random()*4;
+            let spikeLength = (freqData[i] / 255) * sunRadius / 80;
+            spikeLength = spikeLength**4;
+            spikeLength += Math.random()*4;
             totalHeight += freqData[i];
 
-            let spikeWidth = (1-(i/bufferLength)) * 8;
+            let spikeWidth = (1-(i/bufferLength)) * 10;
 
             ctx.moveTo(0, sunRadius);
             ctx.lineTo(-spikeWidth/2, sunRadius);
-            ctx.lineTo(0, sunRadius + barHeight);
+            ctx.lineTo(0, sunRadius + spikeLength);
             ctx.lineTo(spikeWidth, sunRadius);
 
             ctx.rotate(i+Math.random()/nudgeFactor);
         }
 
         nudgeFactor = (1-(totalHeight/bufferLength/255))*1500;
-        console.log(nudgeFactor)
 
         let lightness = ((totalHeight/bufferLength/255) * 30) + 70;
 
-        ctx.fillStyle = `hsl(48 100 ${lightness})`;
-        ctx.shadowColor = `hsl(48 100 ${lightness})`;
+        ctx.fillStyle = `hsl(40 100 ${lightness})`;
+        ctx.shadowColor = `hsl(40 100 ${lightness})`;
 
         ctx.fill()
 
@@ -74,7 +78,8 @@ navigator.mediaDevices.getUserMedia({audio: true}).then(function(stream){
 
         ctx.restore()
 
-	}
+    }
 
-	drawFrame();
-})
+    drawFrame();
+});
+
